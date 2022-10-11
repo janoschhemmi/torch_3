ab09794b6d38e68a7dc8a3e9cfeeb0e3ef1d0c2e
janoschhemmi <jan.hemmerling@t-online.de>
2022-10-11 13:26:38 +0200

    innit

---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+
+"""
+Author: Jan Hemmerling
+Description: __init__ file  for data package
+"""
+
+from .load_data_set import *
---
+++ data/load_data_set.py
@@ -0,0 +1,8 @@
+import sys
+
+import numpy as np
+from torch.utils.data import Dataset
+
+from .tools import prepare_input
+
+
